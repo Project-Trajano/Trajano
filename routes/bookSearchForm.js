2a800891982e5970b5878529c376866fa4bbea9d
adelcanto <a.delcanto@outlook.com>
2019-11-26 14:02:02 +0100

    Implemented Form for Post with all fields including Locations

--- routes/bookSearchForm.js
+++ routes/bookSearchForm.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const Location = require('../models/location')
 
 router.get('/bookinfo', (req, res, next) => {
-  res.render('bookSearchForm');
+  Location.find()
+    .then(locationFound => {
+      res.render('bookSearchForm', {
+        locationFound
+      });
+    })
+
 });
 
 router.post('/bookinfo', (req, res, next) => {
   const searchInput = (req.body.title).split(' ').join('+');
-  // searchInput = searchInput.split(' ').join('+')
-  console.log(typeof(searchInput))
-  res.render('bookSearchForm', {searchInput})
+  Location.find()
+    .then(locationFound => {
+      res.render('bookSearchForm', {
+        searchInput, locationFound
+      })
+    })
+})
+
+router.post('/bookinfo/save', (req, res, next) => {
+  console.log('////////////////////////////////////////////////////////////')
+  console.log(req.user)
+  console.log('////////////////////////////////////////////////////////////')
+  console.log(req.body)
+  console.log('////////////////////////////////////////////////////////////')
+  
+  res.redirect('/bookinfo')
 })
 
 module.exports = router;
