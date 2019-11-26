bb819d732d26d337b905ae4c1f026da2f833e90f
adelcanto <a.delcanto@outlook.com>
2019-11-26 18:05:46 +0100

    Implemented New Book + My Collection

--- routes/bookSearchForm.js
+++ routes/bookSearchForm.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const Location = require('../models/location')
+const Book = require('../models/book')
 
 router.get('/bookinfo', (req, res, next) => {
   Location.find()
     .then(locationFound => {
-      res.render('bookSearchForm', {
-        locationFound
-      });
+      Book.find({
+          userId: req.user._id
+        })
+        .then((bookFoundbyUser) => {
+          res.render('bookSearchForm', {
+            locationFound,
+            bookFoundbyUser
+          });
+        })
     })
 
 });
@@ -16,20 +23,41 @@ router.post('/bookinfo', (req, res, next) => {
   const searchInput = (req.body.title).split(' ').join('+');
   Location.find()
     .then(locationFound => {
-      res.render('bookSearchForm', {
-        searchInput, locationFound
-      })
+      Book.find({
+          userId: req.user._id
+        })
+        .then((bookFoundbyUser) => {
+          res.render('bookSearchForm', {
+            searchInput,
+            locationFound,
+            bookFoundbyUser
+          })
+        })
+
     })
 })
 
 router.post('/bookinfo/save', (req, res, next) => {
-  console.log('////////////////////////////////////////////////////////////')
-  console.log(req.user)
-  console.log('////////////////////////////////////////////////////////////')
-  console.log(req.body)
-  console.log('////////////////////////////////////////////////////////////')
-  
-  res.redirect('/bookinfo')
+  const newBook = new Book({
+    title: req.body.name,
+    author: req.body.author,
+    genre: req.body.category,
+    rating: req.body.rating,
+    state: req.body.state,
+    description: req.body.description,
+    locationId: req.body.location,
+    userId: req.user._id,
+    bookImage: {}
+  })
+  newBook.save()
+    .then(() => {
+      res.redirect('/bookinfo')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  // console.log(newBook);
+
 })
 
 module.exports = router;
