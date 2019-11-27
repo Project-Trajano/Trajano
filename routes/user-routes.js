723e3f63d24c7218e5fbfd6e4e95d56c2475c8c5
enriqueMontano <montanoenrique@outlook.com>
2019-11-27 13:31:41 +0100

    models names fixed

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const ensureLogin = require("connect-ensure-login");
-const User = require("../models/User");
+const User = require("../models/user");
 const multer = require("multer");
 const uploadCloud = require("../config/cloudinary.js");
 const upload = multer({ dest: "../public/uploads/" });
